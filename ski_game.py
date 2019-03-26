2ea1d3375c5991859cabaa0328aaafc0013e04da
huangbinz <1649653774@qq.com>
2019-03-26 11:51:39 +0800

    version3

--- ski_game.py
+++ ski_game.py
@@ -1,113 +1,117 @@
-"""小雪人滑雪"""
 import pygame as pg
+import random
+import time
 pg.init()
 
-class Game(object):
-    def __init__(self, win_x, win_y, title):
-        self.win_x = win_x      #窗口宽
-        self.win_y = win_y      #窗口高
-        self.windown = pg.display.set_mode((self.win_x, self.win_y))
-        pg.display.set_caption(title)   #设置窗口标题
 
-    def win_bk_color(self, color):
-        """设置游戏窗口背景颜色、标题和尺寸"""
-        self.windown.fill([color[0], color[1], color[2]])
+class Skier(pg.sprite.Sprite):
+    """创建滑雪小人"""
+    def __init__(self, image, location, speed):
+        """
+        :param image: 图片路径
+        :param location: 图片的初始位置
+        :param speed: 对象的移动速度
+        """
+        super().__init__()
+        self.image = pg.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+        self.speed = speed
 
-    def win_music(self, music_path):
-        """设置游戏音乐"""
-        pg.mixer.music.load(music_path)
-        if pg.mixer.music.get_busy() == False:
-            pg.mixer.music.play()
-
-    def game_over(self):
-        """结束游戏"""
-        ret = pg.event.get()
-        for obj in ret:
-            if obj.type == pg.QUIT:
-                print("shut down the window.")
-                exit()
-
-
-
-class Tree(object):
-    pass
-
-
-
-class Skier(object):
-    def __init__(self, windown, win_x, win_y):
-        self.skier_pic_x = 30  # 雪人图片宽
-        self.skier_pic_y = 64  # 雪人图片高
-        self.skier_x = (win_x - self.skier_pic_x)//2     #雪人位置x
-        self.skier_y = 0            #雪人位置y
-        self.win_x = win_x          #游戏窗口x
-        self.win_y = win_y          #游戏窗口y
-        self.windown = windown      #游戏窗口
-        self.skier_speed = 0.3      #雪人速度
-
-    def display_skier(self, skier_pic_path):
-        """显示雪人"""
-        skier = pg.image.load(skier_pic_path).convert()
-        self.windown.blit(skier, (self.skier_x, self.skier_y))
+    def move(self):
+        keystatus = pg.key.get_pressed()
+        #左移
+        if keystatus[pg.K_LEFT] or keystatus[pg.K_a]:
+            self.rect.left -= self.speed[0]
+            if self.rect.left < 0:
+                self.rect.left = 0
+        #右移
+        if keystatus[pg.K_RIGHT] or keystatus[pg.K_d]:
+            self.rect.right += self.speed[0]
+            if self.rect.right > 600:
+                self.rect.right = 600
+        #下移
+        if keystatus[pg.K_DOWN] or keystatus[pg.K_s]:
+            self.rect.bottom += self.speed[1]
+            if self.rect.bottom > 650:
+                self.rect.bottom = 650
+        #上移
+        if keystatus[pg.K_UP] or keystatus[pg.K_w]:
+            self.rect.top -= self.speed[1]
+            if self.rect.top < 0:
+                self.rect.top = 0
 
 
-    def skier_down(self, skier_pic_path):
-        """雪人向下移动"""
-        self.skier_y += self.skier_speed
-        if self.skier_y >= self.win_y - self.skier_pic_y:
-            self.skier_y = self.win_y - self.skier_pic_y
-        self.display_skier(skier_pic_path)
+class FlagTree(object):
+    """创建一个旗帜或树"""
+    def __init__(self, image, location, speed):
+        """
+        :param image: 图片路径
+        :param location: 图片的初始位置
+        :param speed: 对象的移动速度
+        """
+        super().__init__()
+        self.image = pg.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+        self.speed = speed
 
-    def skier_up(self, skier_pic_path):
-        """雪人向上移动"""
-        self.skier_y -= self.skier_speed
-        if self.skier_y <= 0:
-            self.skier_y = 0
-        self.display_skier(skier_pic_path)
-
-    def skier_left(self, skier_pic_path):
-        """雪人向左移动"""
-        self.skier_x -= self.skier_speed
-        if self.skier_x <= 0:
-            self.skier_x = 0
-        self.display_skier(skier_pic_path)
-
-    def skier_right(self, skier_pic_path):
-        """雪人向右移动"""
-        self.skier_x += self.skier_speed
-        if self.skier_x >= self.win_x - self.skier_pic_x:
-            self.skier_x = self.win_x - self.skier_pic_x
-        self.display_skier(skier_pic_path)
-
-
+    def move(self):
+        self.rect = self.rect.move(self.speed)
 
 
 if __name__ == '__main__':
-    game = Game(600, 650, "ski game")   #创建游戏
-    skier = Skier(game.windown, game.win_x, game.win_y)         #创建雪人
+    def delay(num):
+        """自定义延时"""
+        for i in range(num):
+            for j in range(10):
+                pass
+
+    window = pg.display.set_mode((600, 650))
+    window.fill([255, 255, 255])
+
+    skier = Skier("./pictures/skier_down.png", (285, 2), [1, 1])
+    tree1 = FlagTree("./pictures/skier_tree.png", (random.randint(0, 570), random.randint(650, 1500)), [0, -1])
+    tree2 = FlagTree("./pictures/skier_tree.png", (random.randint(0, 570), random.randint(650, 1500)), [0, -1])
+    tree3 = FlagTree("./pictures/skier_tree.png", (random.randint(0, 570), random.randint(650, 1500)), [0, -1])
+    tree4 = FlagTree("./pictures/skier_tree.png", (random.randint(0, 570), random.randint(650, 1500)), [0, -1])
+    tree5 = FlagTree("./pictures/skier_tree.png", (random.randint(0, 570), random.randint(650, 1500)), [0, -1])
+    tree6 = FlagTree("./pictures/skier_tree.png", (random.randint(0, 570), random.randint(650, 1500)), [0, -1])
+    tree7 = FlagTree("./pictures/skier_tree.png", (random.randint(0, 570), random.randint(650, 1500)), [0, -1])
+    tree8 = FlagTree("./pictures/skier_tree.png", (random.randint(0, 570), random.randint(650, 1500)), [0, -1])
+    flag1 = FlagTree("./pictures/skier_flag.png", (random.randint(0, 580), random.randint(650, 1200)), [0, -1])
+    flag2 = FlagTree("./pictures/skier_flag.png", (random.randint(0, 580), random.randint(650, 1200)), [0, -1])
+    flag3 = FlagTree("./pictures/skier_flag.png", (random.randint(0, 580), random.randint(650, 1200)), [0, -1])
+    tree = [tree1, tree2, tree3,tree4,tree5,tree6,tree7,tree8]
+    flag = [flag1, flag2, flag3]
+
 
     while 1:
-        game.win_bk_color([0, 255, 0])
-        # game.win_music("./music/bg_music.mp3")
-        skier.display_skier("./pictures/skier_down.png")
+        window.fill([255, 255, 255])
 
-        keystatus = pg.key.get_pressed()  # 检查键盘所有按键的状态（布尔型）
+        skier.move()
+        window.blit(skier.image, skier.rect)
 
-        if keystatus[pg.K_DOWN] or keystatus[pg.K_s]:
-            skier.skier_down("./pictures/skier_down.png")
-        if keystatus[pg.K_UP] or keystatus[pg.K_w]:
-            skier.skier_up("./pictures/skier_down.png")
-        if keystatus[pg.K_LEFT] or keystatus[pg.K_a]:
-            skier.skier_left("./pictures/skier_down.png")
-        if keystatus[pg.K_RIGHT] or keystatus[pg.K_d]:
-            skier.skier_right("./pictures/skier_down.png")
+        #树
+        for i in range(8):
+            tree[i].move()
+            window.blit(tree[i].image, tree[i].rect)
+            # time.sleep(0.00001)
+            delay(1000)
+            if tree[i].rect.bottom < 0:
+                tree[i].rect.top = random.randint(650, 1500)
+                tree[i].rect.left = random.randint(0, 570)
 
+        #旗
+        for i in range(3):
+            flag[i].move()
+            window.blit(flag[i].image, flag[i].rect)
+            if flag[i].rect.bottom < 0:
+                flag[i].rect.top = random.randint(650, 1200)
+                flag[i].rect.left = random.randint(0, 570)
 
-
-
-        game.game_over()
+        #结束游戏
+        for obj in pg.event.get():
+            if obj.type == pg.QUIT:
+                exit()
         pg.display.update()
-
-
-
-
